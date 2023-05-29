a7cca20e1bbb0b37490e2d5e241bc0c75cc1fcd5
juned-shaikh <juned.shaikh.7415@gmail.com>
2023-05-29 11:54:59 +0530

    'sd'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
-}
+  amount:number=0;
+  fromCurrency: string='USD';
+  toCurrency: string='INR';
+  convertedAmount: any;
+  exchangeRates:any;
+  conversionRate:any;
+  NewConversionRate:any;
+
+  constructor(public http:HttpClient){}
+
+
+  convert(){
+ 
+    this.http.get<any>(`https://api.exchangerate-api.com/v4/latest/${this.fromCurrency}`).subscribe(data => {
+       this.exchangeRates = data.rates;
+       this.conversionRate = this.exchangeRates[this.toCurrency];
+
+      this.convertedAmount = this.amount * this.conversionRate;
+      this.NewConversionRate=this.convertedAmount.toFixed(2)
+      console.log(data)
+    });
+  }
+  }
+
+
+
+
